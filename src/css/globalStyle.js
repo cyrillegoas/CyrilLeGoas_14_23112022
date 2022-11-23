82f60d15e543a979e63418424f57bed740b30bcc
Cyril Le Goas <cyril.legoas@gmail.com>
2022-11-23 23:12:42 +0100

    FIX css properties

--- src/css/globalStyle.js
+++ src/css/globalStyle.js
@@ -1,19 +1,19 @@
 import { globalCss } from '../../stitches.config';
 
 export const globalStyle = globalCss({
-  '*, *::before, *::after': { 'box-sizing': 'border-box' },
+  '*, *::before, *::after': { boxSizing: 'border-box' },
   '*': { margin: 0 },
   'html, body': { height: '100%' },
   'img, picture, video, canvas, svg': {
     display: 'block',
-    'max-width': '100%',
+    maxWidth: '100%',
   },
   'input, button, textarea, select': { font: 'inherit' },
-  'p, h1, h2, h3, h4, h5, h6': { 'overflow-wrap': 'break-word' },
+  'p, h1, h2, h3, h4, h5, h6': { overflowWrap: 'break-word' },
   body: {
     color: '$colors$text',
     backgroundColor: '$colors$background',
-    'line-height': '1.5',
+    lineHeight: '1.5',
     '-webkit-font-smoothing': 'antialiased',
   },
   '#root': {
